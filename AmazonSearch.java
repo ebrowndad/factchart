0372abd5b314ac75aa1a491cadfe4f1169da3e3f
Dawn W <Dawn W>
2016-11-30 09:16:20 -0500

    Updated AmazonSearch file

--- AmazonSearch.java
+++ AmazonSearch.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-//import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
